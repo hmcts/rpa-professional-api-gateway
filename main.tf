59f697e799e5262db81c9f28bf6f7d08427cf6d8
alexis.gayte <alexis.gayte@gmail.com>
2018-08-20 16:47:56 +0100

    give a try

--- main.tf
+++ main.tf
@@ -8,17 +8,6 @@ data "template_file" "papi_template" {
   template = "${file("${path.module}/templates/professional-api-management.json")}"
 }
 
-resource "azurerm_subnet" "api-papi-subnet" {
-  name                 = "core-infra-subnet-papi-${var.env}"
-  resource_group_name  = "${var.vnet_rg_name}"
-  virtual_network_name = "${var.vnet_name}"
-  address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
-
-  lifecycle {
-    ignore_changes     = "address_prefix"
-  }
-}
-
 resource "azurerm_template_deployment" "papi-managment" {
   template_body       = "${data.template_file.papi_template.rendered}"
   name                = "${local.name}"
@@ -32,7 +21,6 @@ resource "azurerm_template_deployment" "papi-managment" {
     notification_sender_email          = "${var.notification_sender_email}"
     env                                = "${var.env}"
     platform_papi_name                 = "${local.name}"
-    platform_papi_subnetResourceId     = "${azurerm_subnet.api-papi-subnet.id}"
     platform_papi_sku                  = "${local.platform_api_papi_sku}"
   }
 }
