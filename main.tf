57090f542bec3b8ed4da4c16c5ebf99093f444b0
alexis.gayte <alexis.gayte@gmail.com>
2018-10-03 13:07:37 +0100

    chaneg name + indentation

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ resource "azurerm_template_deployment" "papi-managment" {
     platform_papi_sku                          = "${local.platform_api_papi_sku}"
     oauth_token_endpoint                       = ""
     oauth_authorization_endpoint_redirect_uri  = ""
-    oauth_authorization_endpoint  = ""
-    oauth_client_id  = ""
-    oauth_client_secret  = ""
+    oauth_authorization_endpoint               = ""
+    oauth_client_id                            = "papi"
+    oauth_client_secret                        = "xxxxxxxxx"
   }
 }
