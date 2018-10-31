6e4e757ba7505fb1318b245f447b1cc9617afe1b
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-31 16:44:06 +0000

    Use AAT for vnet not sandbox

--- main.tf
+++ main.tf
@@ -36,8 +36,8 @@ module "papi-managment" {
   
   location                                   = "${var.location}"
   env                                        = "${var.env}"
-  vnet_rg_name                               = "core-infra-vnet-${var.env}"
-  api_subnet_id                              = "core-infra-subnet-apimgmt-${var.env}"
+  vnet_rg_name                               = "core-infra-vnet-${local.local_env}"
+  api_subnet_id                              = "core-infra-subnet-apimgmt-${local.local_env}"
   publisher_email                            = "${data.azurerm_key_vault_secret.publisher_email.value}"
   # publisher_name
   # notification_sender_email
