9550ffb2d81b338d6bd4f0b266a75a10c3608339
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-31 16:31:44 +0000

    Fix terraform output variables

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "host_name" {
-  value = "${azurerm_template_deployment.papi-managment.name}.azure-api.net"
+  value = "${module.papi-managment.name}.azure-api.net"
 }
 
 output "developer_portal_url" {
-  value = "${azurerm_template_deployment.papi-managment.name}.portal.azure-api.net"
+  value = "${module.papi-managment.name}.portal.azure-api.net"
 }
