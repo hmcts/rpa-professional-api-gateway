a38429fc8ebb76ffdfed534cf9b9da0cd68b9da0
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-31 16:17:43 +0000

    Use my copy of CNP API mgmt terraform module to create service

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 locals {
   name                  = "rpa-professional-api-portal-${var.env}"
   platform_api_papi_sku = "${var.env == "prod" ? "Premium" : "Developer"}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 }
 
 
@@ -30,31 +31,29 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location}"
 }
 
-data "template_file" "papi_template" {
-  template = "${file("${path.module}/templates/professional-api-management.json")}"
-}
-
-resource "azurerm_template_deployment" "papi-managment" {
-  template_body       = "${data.template_file.papi_template.rendered}"
-  name                = "${local.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  deployment_mode     = "Incremental"
-
-  parameters = {
-    location                                   = "${var.location}"
-    publisher_email                            = "${data.azurerm_key_vault_secret.publisher_email.value}"
-    env                                        = "${var.env}"
-    platform_papi_name                         = "${local.name}"
-    platform_papi_infra           			   = "core-infra-${var.env}"
-    platform_papi_vnet           			   = "core-infra-vnet-${var.env}"
-    platform_papi_snet   	        		   = "core-infra-subnet-apimgmt-${var.env}"
-    subscription_id                            = "${var.subscription_id}"
-    platform_papi_sku                          = "${local.platform_api_papi_sku}"
-    oauth_token_endpoint                       = "${var.oauth_token_endpoint}"
-    oauth_authorization_endpoint_redirect_uri  = "${var.oauth_authorization_endpoint_redirect_uri}"
-    oauth_client_registration_endpoint         = "${var.oauth_client_registration_endpoint}"
-    oauth_authorization_endpoint               = "${var.oauth_authorization_endpoint}"
-    oauth_client_id                            = "${data.azurerm_key_vault_secret.oauth2_client.value}"
-    oauth_client_secret                        = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
-  }
+module "papi-managment" {
+  source              = "git@github.com:TabbyC/cnp-module-api-mgmt?ref=remove_echo_api"
+  
+  location                                   = "${var.location}"
+  env                                        = "${var.env}"
+  # vnet_rg_name
+  # api_subnet_id
+  publisher_email                            = "${data.azurerm_key_vault_secret.publisher_email.value}"
+  # publisher_name
+  # notification_sender_email
+  # common_tags
+  oauth2_token_endpoint                       = "${var.oauth_token_endpoint}"
+  oauth2_authorization_endpoint_redirect_uri  = "${var.oauth_authorization_endpoint_redirect_uri}"
+  oauth2_client_registration_endpoint         = "${var.oauth_client_registration_endpoint}"
+  oauth2_authorization_endpoint               = "${var.oauth_authorization_endpoint}"
+  oauth2_client_id                            = "${data.azurerm_key_vault_secret.oauth2_client.value}"
+  oauth2_client_secret                        = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
+  
+  ## params not in module (yet) ##
+  # platform_papi_name                         = "${local.name}"
+  # platform_papi_infra           			   = "core-infra-${var.env}"
+  # platform_papi_vnet           			   = "core-infra-vnet-${var.env}"
+  # platform_papi_snet   	        		   = "core-infra-subnet-apimgmt-${var.env}"
+  # subscription_id                            = "${var.subscription_id}"
+  # platform_papi_sku                          = "${local.platform_api_papi_sku}"
 }
